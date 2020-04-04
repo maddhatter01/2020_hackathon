9284d2ec14776b5a3ede60dba14b281fe50d7416
maddhatter01 <powellt1@sou.edu>
2020-04-04 16:49:30 -0700

    Mt Shasta - Hackathon

--- mtShasta/src/mtShasta/Driver.java
+++ mtShasta/src/mtShasta/Driver.java
@@ -2,10 +2,10 @@ package mtShasta;
 
 import java.util.Scanner;
 
-public class Driver extends Questions{
+public class Driver {
 	public static void main(String[] args) {
 		int score = 0;
-		int qNum = 1;
+		int questionNum = 1;
 		Object question;
 		
 		Scanner scan = new Scanner(System.in);
@@ -14,7 +14,7 @@ public class Driver extends Questions{
 		
 		// Question 1. Enter the question, and all possible answer, and the correct answer
 	    q1 = new Questions();
-	    q1.setQ(1, "What is the elevation of Mt. Shasta?");
+	    q1.setQ("What is the elevation of Mt. Shasta?");
 	    q1.setO(1, "14,180 ft");
 	    q1.setO(2, "23,152 ft");
 	    q1.setO(3, "9,122 ft");
@@ -23,7 +23,7 @@ public class Driver extends Questions{
 	   
 	    // Question 2. Enter the question, and all possible answer, and the correct answer
 	    q2 = new Questions();
-	    q2.setQ(2, "Which mountain range is Mt. Shasta located in?");
+	    q2.setQ("Which mountain range is Mt. Shasta located in?");
 	    q2.setO(1, "Rocky Mountain");
 	    q2.setO(2, "Sierra");
 	    q2.setO(3, "Cascade");
@@ -32,7 +32,7 @@ public class Driver extends Questions{
 	    
 	    // Question 3. Enter the question, and all possible answer, and the correct answer
 	    q3 = new Questions();
-	    q3.setQ(3, "What type of volcano is Mt. Shasta?");
+	    q3.setQ("What type of volcano is Mt. Shasta?");
 	    q3.setO(1, "Shield volcano");
 	    q3.setO(2, "Stratovolcano");
 	    q3.setO(3, "Pyroclastic cone");
@@ -41,7 +41,7 @@ public class Driver extends Questions{
 
 	    // Question 4. Enter the question, and all possible answer, and the correct answer
 	    q4 = new Questions();
-	    q4.setQ(4, "When did Mt. Shasta last erupt?");
+	    q4.setQ("When did Mt. Shasta last erupt?");
 	    q4.setO(1, "1902");
 	    q4.setO(2, "1786");
 	    q4.setO(3, "1593");
@@ -50,7 +50,7 @@ public class Driver extends Questions{
 
 	    // Question 5. Enter the question, and all possible answer, and the correct answer
 	    q5 = new Questions();
-	    q5.setQ(5, "What year was Mt. Shasta designated a US national natural landmark?");
+	    q5.setQ("What year was Mt. Shasta designated a US national natural landmark?");
 	    q5.setO(1, "1938");
 	    q5.setO(2, "1952");
 	    q5.setO(3, "1898");
@@ -61,42 +61,46 @@ public class Driver extends Questions{
 	    question = q1;
 	    
 	    // Loop 5 times for all five questions, incrementing the gNum var to move to the next question.
-	    while(qNum<=5) {
-	    	((Questions) question).showQ(qNum);
+	    while(questionNum<=5) {
+	    	((Questions) question).showQ(questionNum);
 	    	System.out.println("\nEnter 1, 2, 3, or 4 -> ");
 	    	int userAnswer = scan.nextInt();
 	    	
 	    	// If correct, increment the score, and display Correct!
 	    	if(((Questions) question).getAnswer()==userAnswer) {
 	    		score++;
-	    		System.out.println("Correct!\n");
+	    		System.out.println("--------------\nCorrect!\n--------------");
 	    	}
 	    	// If incorrect display Nope!
 	    	else{
-	    		System.out.println("Nope!\n");
+	    		System.out.println("--------------\nNope!\n--------------");
 	    	}
 	    	// increment qNum
-	    	qNum++;
+	    	questionNum++;
 	    	// Change the reference of the object in the question var to move on to the next question, based on qNum
-	    	if(qNum == 2) {
+	    	if(questionNum == 2) {
 	    		question = q2;
 	    	}
 	    	// Change the reference of the object in the question var to move on to the next question, based on qNum
-	    	if(qNum == 3) {
+	    	if(questionNum == 3) {
 	    		question = q3;
 	    	}
 	    	// Change the reference of the object in the question var to move on to the next question, based on qNum
-	    	if(qNum == 4) {
+	    	if(questionNum == 4) {
 	    		question = q4;
 	    	}
 	    	// Change the reference of the object in the question var to move on to the next question, based on qNum
-	    	if(qNum == 5) {
+	    	if(questionNum == 5) {
 	    		question = q5;
 	    	}
-	  
 	    }
-	    // Print out the score, and close the scanner
-	    System.out.println("Your score is: " + score);
+	    // Print out the results, and close the scanner
+	    if(score==5) {
+	    	System.out.println("\nWell Done!! Perfect Score!!!");
+	    }
+	    else {
+	    	System.out.println("\nYou got " + score + " out of 5 correct");
+	    }
 	    scan.close();
 	}
 }
--- mtShasta/src/mtShasta/Questions.java
+++ mtShasta/src/mtShasta/Questions.java
@@ -2,98 +2,58 @@ package mtShasta;
 
 public class Questions {
 	
-private String opt1, opt2, opt3, opt4;
-private String q1, q2, q3, q4, q5;
-private int answer;
-
-// Set the multiple choice options for a question, all question have 4 possible answers
-public void setO(int i, String s) 
-{
-    if (i == 1) 
-    {
-        opt1 = s;
-    }
-    if (i == 2) 
-    {
-        opt2 = s;
-    }
-    if (i == 3) 
-    {
-        opt3 = s;
-    }
-    if (i == 4) {
-        opt4 = s;
-    }
-}
-
-// Set the question
-public void setQ(int i, String s) 
-{
-    if (i == 1) 
-    {
-        q1 = s;
-    }
-    if (i == 2) 
-    {
-        q2 = s;
-    }
-    if (i == 3) 
-    {
-        q3 = s;
-    }
-    if (i == 4) 
-    {
-        q4 = s;
-    }
-    if (i == 5) 
-    {
-        q5 = s;
-    }
-  
-}
-
-// Set the answer for the question, all answers are indicated by the option # associated with them. 
-public void setAnswer(int i) 
-{
-    answer = i;
-}
-
-// Display the question with each possible answer
-public void showQ(int i) 
-{
-    System.out.println(getQ(i));
-    System.out.println("1. " + opt1 + "\n2. " + opt2 + "\n3. " + opt3 + "\n4. " + opt4);
-}
-
-// Get the answer for a specific question
-public int getAnswer() 
-{
-    return answer;
-}
-
-// Get what the question is
-public String getQ(int i) 
-{
-    if (i == 1) 
-    {
-        return q1;
-    }
-    if (i == 2) 
-    {
-        return q2;
-    }
-    if (i == 3) 
-    {
-        return q3;       
-    }
-    if (i == 4) 
-    {
-        return q4;
-    }
-    if (i == 5) 
-    {
-        return q5;
-    }
-    return "Something went wrong, please try again later.";
+	private String opt1, opt2, opt3, opt4;
+	private String question;
+	private int answer;
+	
+	// Set the multiple choice options for a question, all question have 4 possible answers
+	public void setO(int i, String s) 
+	{
+	    if (i == 1) 
+	    {
+	        opt1 = s;
+	    }
+	    if (i == 2) 
+	    {
+	        opt2 = s;
+	    }
+	    if (i == 3) 
+	    {
+	        opt3 = s;
+	    }
+	    if (i == 4) {
+	        opt4 = s;
+	    }
+	}
+	
+	// Set the question
+	public void setQ(String s) 
+	{
+		question = s;
+	}
+	
+	// Set the answer for the question, all answers are indicated by the option # associated with them. 
+	public void setAnswer(int i) 
+	{
+	    answer = i;
+	}
+	
+	// Display the question with each possible answer
+	public void showQ(int i) 
+	{
+	    System.out.println(getQ(i));
+	    System.out.println("1. " + opt1 + "\n2. " + opt2 + "\n3. " + opt3 + "\n4. " + opt4);
+	}
+	
+	// Get the answer for a specific question
+	public int getAnswer() 
+	{
+	    return answer;
+	}
+	
+	// Get what the question is
+	private String getQ(int i) 
+	{
+		return question;
 	}
 }
